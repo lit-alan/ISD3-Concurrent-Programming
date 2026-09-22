7898abc1a952498bed85c364de7b8fd2673a4d21
lit-alan <Alan.Ryan@lit.ie>
2026-09-22 12:53:11 +0100

    added starter code for Lab Ex 4

---
+++ todo/exercise_4/LabEx_Four_Starter/src/main/java/model/Customer.java
@@ -0,0 +1,47 @@
+package model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    private Long customerId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private LocalDate dateOfBirth;
+
+    private String paymentMethod;
+
+    private String communicationPreferences;
+
+    private String county;
+
+    @Override
+    public String toString() {
+        return  "Customer ID: " + customerId + "\t" +
+                "Name: " + firstName + " " + lastName + "\t" +
+                "Address: " + address + ", " + county + "\t" +
+                "Date of Birth: " + dateOfBirth + "\t" +
+                "Payment Method: " + paymentMethod + "\t" +
+                "Email: " + email;
+    }
+
+
+}
